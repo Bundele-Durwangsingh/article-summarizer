bb0830d2ca198acea45d57311611fe973f822f58
DRAG-11 <bundeledurwang@gmail.com>
2022-12-30 18:51:31 +0530

    final commit

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import nltk
 from textblob import TextBlob
 from newspaper import Article
 
+
 def summerize():
-    url=urltext.get('1.0','end').strip()
+    url = utext.get('1.0', 'end').strip()
     article = Article(url)
     article.download()
     article.parse()
@@ -12,35 +13,29 @@ def summerize():
 
     title.config(state='normal')
     author.config(state='normal')
-    publicatin_date.config(state='normal')
+    publication.config(state='normal')
     summary.config(state='normal')
-    sentiments.config(state='normal')
+
+    title.delete('1.0', 'end')
+    title.insert('1.0', article.title)
+
+    author.delete('1.0', 'end')
+    author.insert('1.0', article.authors)
+
+    publication.delete('1.0', 'end')
+    publication.insert('1.0', article.publish_date)
+
+    summary.delete('1.0', 'end')
+    summary.insert('1.0', article.summary)
+
     
-    title.delete('1.0','end')
-    title.insert('1.0',article.title)
-
-    author.delete('1.0','end')
-    author.insert('1.0',article.authors)
-
-    publicatin_date.delete('1.0','end')
-    publicatin_date.insert('1.0',article.publish_date)
-
-    summary.delete('1.0','end')
-    summary.insert('1.0',article.summary)
-
-    analysis = TextBlob(article.text)
-    sentiments.delete('1.0','end')
-    sentiments.insert('1.0'f'polarity : {analysis.polarity},Sentiments :{"Positive" if analysis.polarity >0 else "Negative" if analysis.polarity<0 else "Netural"}')
 
     title.config(state='disabled')
     author.config(state='disabled')
-    publicatin_date.config(state='disabled')
+    publication.config(state='disabled')
     summary.config(state='disabled')
-    sentiments.config(state='disabled')
 
 
-
-    
 
 # Bulding GUI
 root = tk.Tk()
@@ -61,27 +56,21 @@ author.pack()
 # Publication date
 plabel = tk.Label(root, text='Publication Date')
 plabel.pack()
-publicatin_date = tk.Text(root, height=1, width=140)
-publicatin_date.config(state='disable', bg='#D3D3D3')
-publicatin_date.pack()
+publication = tk.Text(root, height=1, width=140)
+publication.config(state='disable', bg='#D3D3D3')
+publication.pack()
 # summary
 slabel = tk.Label(root, text='Summary')
 slabel.pack()
 summary = tk.Text(root, height=20, width=140)
 summary.config(state='disable', bg='#D3D3D3')
 summary.pack()
-# Sentiments
-selabel = tk.Label(root, text='Sentiments Analysis')
-selabel.pack()
-sentiments = tk.Text(root, height=1, width=140)
-sentiments.config(state='disable', bg='#D3D3D3')
-sentiments.pack()
 # url
 ulabel = tk.Label(root, text='URL')
 ulabel.pack()
-urltext = tk.Text(root, height=1, width=140)
-urltext.pack()
+utext = tk.Text(root, height=1, width=140)
+utext.pack()
 # Button
-btn = tk.Button(root, text='Submit',command=summerize)
+btn = tk.Button(root, text='Submit', command=summerize)
 btn.pack()
 root.mainloop()
